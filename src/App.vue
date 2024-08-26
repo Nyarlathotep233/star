e804b59773ac5f4b4fb7bd0779c3082715c2cd87
Nyarlathotep233 <809679663@qq.com>
2024-08-26 23:12:57 +0800

    fix: 把内容居中

--- src/App.vue
+++ src/App.vue
@@ -16,26 +16,63 @@ export default {
   components: {
     // HelloWorld
     StarRating,
-    FullScreenToggle
-  }
-}
+    FullScreenToggle,
+  },
+};
 </script>
 
 <style>
+html,
+body,
 #app {
-  width: 100vw;
-  height: 100vh;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  padding-top: 60px;
   /* 设置背景图片 */
   background-image: url('@/assets/bkg.jpg');
   background-size: cover; /* 确保背景图片覆盖整个容器 */
   background-position: center; /* 将背景图片居中 */
   background-repeat: no-repeat; /* 防止背景图片重复 */
   background-attachment: fixed; /* 背景图片固定，不随页面滚动 */
+}
+
+.container {
+  width: 100%;
+}
+
+.star-container {
+  cursor: pointer;
+  display: inline-block;
+  margin: 10px;
+  position: relative;
+}
+
+.star {
+  font-size: 60px;
+  color: #ccc;
+  transition: color 0.3s;
+}
+
+.star.active {
+  color: gold;
+}
+
+.name {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(-50%, -50%, 0);
+  font-weight: bold;
+  font-size: 14px;
+  color: black;
 }
 </style>
